30ef5e4c37f26da6e7fedf687eee53137b550c77
u084785 <jonathan@ohrstrom.nu>
2025-04-28 09:53:18 +0200

    Minor improvements to exceptions Improved example project

---
+++ Jonteohr.BugSharp/Exceptions/BugZillaException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BugSharp.Exceptions
+{
+    /// <summary>
+    /// The generic exception for whenever some configuration of the client is invalid.
+    /// </summary>
+    public class BugZillaException : Exception
+    {
+        /// <summary>
+        /// Create a new exception
+        /// </summary>
+        public BugZillaException(string message = null, Exception innerException = null) : base(message, innerException)
+        {
+        }
+    }
+}
